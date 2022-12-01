8aa51c1169f94d627ae725a977cb464f7fe04bea
bircni <nicolas@bircks.eu>
2022-12-01 10:52:04 +0100

    renamed Day1 source class

--- src/coding/day01/Day01.java
+++ src/coding/day01/Day01.java
@@ -6,21 +6,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Day01_a {
+public class Day01 {
 
     static final ArrayList<Integer> elves = new ArrayList<>();
     static int elf = 0;
+
     public static void main(String[] args)
     {
-        try{
+        try {
             File input = new File("src/coding/day01/input.txt");
             Scanner s = new Scanner(input);
             while (s.hasNextLine()) {
                 String data = s.nextLine();
                 if (data.matches("[0-9]+")) {
                     elf = elf + Integer.parseInt(data);
-                }
-                else {
+                } else {
                     elves.add(elf);
                     elf = 0;
                 }
@@ -34,7 +34,7 @@ public class Day01_a {
         System.out.println(result);
         // Exercise b
         int max3 = 0;
-        int [] elves3 = new int[3];
+        int[] elves3 = new int[3];
         ArrayList<Integer> elvesCopy = new ArrayList<>(elves);
         for (int i = 0; i < 3; i++) {
             max3 = max3 + Collections.max(elvesCopy);
