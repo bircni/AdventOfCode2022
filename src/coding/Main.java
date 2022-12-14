1a81fa20fc259cb21a52cc2ad87700e4913ad297
bircni <nicolas@bircks.eu>
2022-12-14 19:23:16 +0100

    added Main

---
+++ src/coding/Main.java
@@ -0,0 +1,25 @@
+package coding;
+
+public class Main {
+
+    /**
+     * Used to run the code for all done days
+     *
+     * @param args not used
+     */
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 24; i++) {
+            String day = "Day0" + (i);
+            String dayURL = "coding." + day.toLowerCase() + "." + day;
+            try {
+                Class.forName(dayURL).getName();
+                System.out.println("Running " + day + " :");
+                Class.forName(dayURL).getMethod("main", String[].class).invoke(null, (Object) args);
+                System.out.println();
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
+}
